2bac39e80ffeca5dfd2ae0285629a7aecfdebd2b
Cameron Graybill <cdgraybill@gmail.com>
2020-02-24 19:58:24 -0600

    added ID column under Products tab

---
+++ InClassWebApplication/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClassWebApplication.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+}
